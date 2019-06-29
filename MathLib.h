44d21dc7181684c75e9fed857fb16515ef1e79f2
binhle18122000 <binhb1807541@student.ctu.edu.vn>
2019-06-29 14:35:38 +0700

    av

---
+++ MathLib.h
@@ -0,0 +1,11 @@
+#pragma once
+
+int MyAdd(int a, int b)
+{
+	return a + b;
+}
+
+int MyMutiply(int a, int b)
+{
+	return a * b;
+}
---
+++ Source.cpp
@@ -0,0 +1,11 @@
+#include"MathLib.h"
+#include<stdio.h>
+#include<Windows.h>
+
+int main()
+{
+	int result = MyAdd(3, 4);
+	printf("Ket Qua La: %d", result);
+	system("pause");
+	return 0;
+}
